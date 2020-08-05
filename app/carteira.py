e033218c8e052b602d85d8a5be2956c80b047ce9
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-08-05 19:37:49 -0300

    Update carteira.py

--- app/carteira.py
+++ app/carteira.py
@@ -5,3 +5,15 @@ bp_carteira = Blueprint('carteira', __name__)
 @bp_carteira.route('/mostrar', methods=['GET'])
 def mostrar():
     ...
+
+@bp_carteira.route('/deletar', methods=['GET'])
+def deletar():
+    ...
+
+@bp_carteira.route('/modificar', methods=['GET'])
+def modificar():
+    ...
+
+@bp_carteira.route('/inserir', methods=['GET'])
+def inserir():
+    ...
