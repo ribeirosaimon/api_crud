402ac90d1a66c2d3fb7ebf75f24c34305700de48
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-08-06 00:19:42 -0300

    comit

--- app/carteira.py
+++ app/carteira.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, current_app, request
 from .model import Carteira
 from .serealizer import CarteiraSchema
 
@@ -11,13 +11,23 @@ def mostrar():
     return cs.jsonify(result), 200
 
 @bp_carteira.route('/deletar', methods=['GET'])
-def deletar():
-    ...
+def deletar(identificador):
+    Carteiar.query.filter(Carteira.id == identificador).delete()
+    current_app.db.session.commit()
+    return jsonify('Deletado!')
 
-@bp_carteira.route('/modificar', methods=['GET'])
-def modificar():
-    ...
+@bp_carteira.route('/modificar', methods=['POST'])
+def modificar(identificador):
+    cs = CarteiraSchema()
+    query = Carteira.query.filter(Carteira.id == identificador)
+    query.update(request.json)
+    current_app.db.session.commit()
+    return cs.jsonify(query.firts())
 
-@bp_carteira.route('/inserir', methods=['GET'])
-def inserir():
-    ...
+@bp_carteira.route('/cadastrar', methods=['POST'])
+def cadastrar():
+    cs = CarteiraSchema()
+    carteira, error = cs.load(request.json)
+    current_app.db.session.add(carteira)
+    current_app.db.session.commit()
+    return cs.jsonify(carteira), 201
